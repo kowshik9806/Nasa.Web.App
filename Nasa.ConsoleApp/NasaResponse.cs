4cb2b7d37ea7163ed8c8963cd3aa65df8d9fb899
QWEqazwsx123@ <koushik9806@gmail.com>
2024-03-06 08:47:21 +0530

    Added Images Logic

---
+++ Nasa.ConsoleApp/NasaResponse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Nasa.ConsoleApp
+{
+    public class Camera
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int rover_id { get; set; }
+        public string full_name { get; set; }
+    }
+
+    public class Camera2
+    {
+        public string name { get; set; }
+        public string full_name { get; set; }
+    }
+
+    public class Photo
+    {
+        public int id { get; set; }
+        public int sol { get; set; }
+        public Camera camera { get; set; }
+        public string img_src { get; set; }
+        public string earth_date { get; set; }
+        public Rover rover { get; set; }
+    }
+
+    public class NasaResponse
+    {
+        public List<Photo> photos { get; set; }
+    }
+
+    public class Rover
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string landing_date { get; set; }
+        public string launch_date { get; set; }
+        public string status { get; set; }
+        public int max_sol { get; set; }
+        public string max_date { get; set; }
+        public int total_photos { get; set; }
+        public List<Camera> cameras { get; set; }
+    }
+}
--- Nasa.ConsoleApp/Program.cs
+++ Nasa.ConsoleApp/Program.cs
@@ -1,13 +1,59 @@
-﻿public class Program
-{
-    public static void Main(string[] args)
-    {
+﻿using Nasa.ConsoleApp;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 
+public class Program
+{
+    public static async void Main(string[] args)
+    {
+        await GetNasaPhotos();
     }
 
 
     public static async Task GetNasaPhotos()
     {
+        using (var httpClient = new HttpClient())
+        {
+            httpClient.BaseAddress = new Uri("https://api.nasa.gov/");
 
+            httpClient.DefaultRequestHeaders.Add("ApiKey", "O7ef7LYc6WIqDzc5zwh1VH38l8eZZ6t0Bu1IDsVh");
+
+            HttpResponseMessage response = await httpClient.GetAsync("endpoint");
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                var content = await response.Content.ReadAsStringAsync();
+                var photoResponse = JsonConvert.DeserializeObject<NasaResponse>(content);
+                if (photoResponse != null && photoResponse.photos.Count > 0)
+                {
+                    foreach (var image in photoResponse.photos)
+                    {
+                        string localPath = "C:\\Users\\vamsh\\source\\repos\\Nasa.Web.App\\Nasa.ConsoleApp\\Images";
+                        await DownloadImageAsync(image.img_src, localPath);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+    }
+
+    public static async Task DownloadImageAsync(string imageUrl, string outputPath)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            byte[] imageData = await httpClient.GetByteArrayAsync(imageUrl);
+            using (FileStream fs = new FileStream(outputPath, FileMode.Create))
+            {
+                await fs.WriteAsync(imageData, 0, imageData.Length);
+            }
+        }
     }
 }
